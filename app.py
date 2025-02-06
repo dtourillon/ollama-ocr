1472b2c227955673d79a5e49224a1c1da3a21180
Dominique Tourillon <dominique.tourillon@cropguard.ai>
2025-02-06 17:30:28 -0500

    tmp

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from PIL import Image
 
 import streamlit as st
 
+from models.anthropic import extract_text as anthropic_extract_text
 from models.ollama import extract_text as ollama_extract_text
 from models.openai import extract_text as openai_extract_text
 
@@ -32,7 +33,7 @@ st.markdown("---")
 with st.sidebar:
     selected_llm_povider = st.selectbox(
         'Which LLM provider should we use?',
-        ["ollama", "openai"],
+        ["ollama", "openai", "anthropic"],
     )
     st.header("Upload Image")
     uploaded_file = st.file_uploader("Choose an image...", type=['png', 'jpg', 'jpeg'])
@@ -49,6 +50,9 @@ with st.sidebar:
                         st.session_state['ocr_result'] = ollama_extract_text(uploaded_file)
                     elif selected_llm_povider == "openai":
                         st.session_state['ocr_result'] = openai_extract_text(uploaded_file)
+                    elif selected_llm_povider == "anthropic":
+                        st.session_state['ocr_result'] = anthropic_extract_text(uploaded_file
+                        )
                 except Exception as e:
                     st.error(f"Error processing image: {str(e)}")
 
